562aa2d525b5a7a0257d3bcbc927e6fe493bdeee
Carlos Saito <carlos@saito.style>
2019-06-20 10:46:49 +0200

    Fix standard errors

--- test/integration/user.test.js
+++ test/integration/user.test.js
@@ -28,7 +28,7 @@ test('should create a new user in canvas', t => {
 test('should create a new user in canvas even without ladokId', t => {
   t.plan(1)
   const kthid = randomstring.generate(8)
-  const ladokId = randomstring.generate(24)
+  const username = `${kthid}_abc`
   const message = {
     kthid,
     'ugClass': 'user',
