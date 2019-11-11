10c6d1269928b236421262f49b054132259d9e1c
Emil Stenberg <esandin@gmail.com>
2019-11-11 14:28:41 +0100

    handle error if token is invalig in monitor page

--- server/systemroutes.js
+++ server/systemroutes.js
@@ -42,12 +42,13 @@ async function checkCanvasStatus () {
 }
 
 async function checkCanvasKey () {
-try{
-  return canvasApi.getRootAccount()
-}catch(e){
-  log.error('Could not use canvas api.')
-  return false
-}
+  try {
+    
+  return await canvasApi.getRootAccount()
+  } catch (e) {
+    log.error('Could not use canvas api.')
+    return false
+  }
 }
 
 async function _monitor (req, res) {
