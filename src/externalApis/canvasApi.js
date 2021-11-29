37e68b3649145a61625bf40fd1336017a2a08b5f
Carlos Saito <carlos@saito.style>
2021-11-29 15:31:39 +0100

    Implement functions to call Canvas API

--- src/externalApis/canvasApi.js
+++ src/externalApis/canvasApi.js
@@ -3,7 +3,7 @@ const CanvasApi = require("@kth/canvas-api").default;
 
 const canvas = new CanvasApi(
   process.env.CANVAS_API_URL,
-  process.env.CANVAS_API_TOKEN
+  process.env.CANVAS_API_KEY
 );
 
 /** Get the Canvas user with a given KTH ID. Returns null if not found */
@@ -25,7 +25,9 @@ async function updateUser(userId, data) {
 }
 
 async function createUser(user) {
-  return canvas.request("accounts/1/users", "POST", user);
+  return canvas
+    .request("accounts/1/users", "POST", user)
+    .then((res) => res.body);
 }
 
 async function getRootAccount() {
@@ -48,6 +50,60 @@ async function sendEnrollments(path) {
   return canvas.sisImport(path);
 }
 
+/* The following functions are used mainly in tests */
+
+async function createCourse(accountId, course) {
+  return canvas
+    .request(`accounts/${accountId}/courses`, "POST", course)
+    .then((r) => r.body);
+}
+
+async function createDefaultSection(course) {
+  return canvas
+    .request(`courses/${course.id}/sections`, "POST", {
+      course_section: {
+        name: `Section for ${course.name}`,
+        sis_section_id: course.sis_course_id,
+        sis_course_id: course.sis_course_id,
+      },
+    })
+    .then((r) => r.body);
+}
+
+async function getCourseEnrollments(courseId) {
+  return canvas.listItems(`courses/${courseId}/enrollments`).toArray();
+}
+
+async function getSectionEnrollments(courseId, sisSectionId) {
+  return canvas
+    .listPages(`courses/${courseId}/enrollments`, {
+      sis_section_id: sisSectionId,
+    })
+    .toArray();
+}
+
+async function pollUntilSisComplete(sisImportId, initialWait = 100) {
+  for (let i = 1; i < 60; i++) {
+    // eslint-disable-next-line no-await-in-loop
+    const sisImport = await canvas
+      .get(`accounts/1/sis_imports/${sisImportId}`)
+      .then((r) => r.body);
+
+    if (sisImport.progress === 100) {
+      return sisImport;
+    }
+
+    const waitingTime = initialWait * 2 ** i;
+
+    // eslint-disable-next-line no-await-in-loop
+    await new Promise((resolve) => setTimeout(resolve, waitingTime));
+  }
+
+  throw new Error(
+    `SIS import ${sisImportId} did not complete after very long time`
+  );
+}
+
 module.exports = {
   getUser,
   updateUser,
@@ -56,4 +112,9 @@ module.exports = {
   getRootAccount,
   updateLogin,
   sendEnrollments,
+  createCourse,
+  createDefaultSection,
+  getCourseEnrollments,
+  getSectionEnrollments,
+  pollUntilSisComplete,
 };
