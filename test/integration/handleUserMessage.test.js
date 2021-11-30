051b7c9789dc91259b34dd514eaa1dc2ebfd66f6
Carlos Saito <carlos@saito.style>
2021-11-30 10:48:04 +0100

    Rename test files

--- test/integration/handleUserMessage.test.js
+++ test/integration/handleUserMessage.test.js
@@ -1,7 +1,7 @@
 const test = require("tape");
 const randomstring = require("randomstring");
 const { handleMessages } = require("./utils");
-const canvasApi = require("../../canvasApi");
+const canvasApi = require("../../src/externalApis/canvasApi");
 
 test("should create a new user in canvas", (t) => {
   t.plan(1);
