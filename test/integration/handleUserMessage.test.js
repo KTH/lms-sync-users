7a612804808cfcb82af905824ccd60c9697a0a6c
Carlos Saito <carlos@saito.style>
2021-12-16 14:12:24 +0100

    Update handleUserMessage tests

--- test/integration/handleUserMessage.test.js
+++ test/integration/handleUserMessage.test.js
@@ -3,8 +3,10 @@ const randomstring = require("randomstring");
 const handleMessage = require("../../src/messageHandlers");
 const canvasApi = require("../../src/externalApis/canvasApi");
 
+// TODO: Create a unit test that checks what is returned by `handleMessage`
+
 test("should create a new user in canvas", async (t) => {
-  t.plan(4);
+  t.plan(1);
   const kthid = randomstring.generate(8);
   const ladokId = randomstring.generate(24);
   const username = `${kthid}_abc`;
@@ -20,25 +22,14 @@ test("should create a new user in canvas", async (t) => {
     ladok3_student_uid: ladokId,
   };
 
-  const result = await handleMessage(message);
-  t.equal(result.type, "user", "handleMessage result.type must be 'user'");
-  t.equal(
-    result.user.action,
-    "create",
-    "handleMessage user.action must be 'create'"
-  );
+  await handleMessage(message);
 
   const user = await canvasApi.getUser(kthid);
   t.ok(user, "User should exist in Canvas");
-  t.equal(
-    user.id,
-    result.user.id,
-    "Created user in Canvas should be same as value returned by handleMessage"
-  );
 });
 
 test("should create a new user in canvas even without ladokId", async (t) => {
-  t.plan(4);
+  t.plan(1);
   const kthid = randomstring.generate(8);
   const username = `${kthid}_abc`;
   const message = {
@@ -52,25 +43,14 @@ test("should create a new user in canvas even without ladokId", async (t) => {
     primary_email: "esandin@gmail.com",
   };
 
-  const result = await handleMessage(message);
-  t.equal(result.type, "user", "handleMessage result.type must be 'user'");
-  t.equal(
-    result.user.action,
-    "create",
-    "handleMessage user.action must be 'create'"
-  );
+  await handleMessage(message);
 
   const user = await canvasApi.getUser(kthid);
   t.ok(user, "User should exist in Canvas");
-  t.equal(
-    user.id,
-    result.user.id,
-    "Created user in Canvas should be same as value returned by handleMessage"
-  );
 });
 
 test("should create a new user of affiliation:member in canvas", async (t) => {
-  t.plan(4);
+  t.plan(1);
   const kthid = randomstring.generate(8);
   const username = `${kthid}_abc`;
   const ladokId = randomstring.generate(24);
@@ -86,25 +66,14 @@ test("should create a new user of affiliation:member in canvas", async (t) => {
     ladok3_student_uid: ladokId,
   };
 
-  const result = await handleMessage(message);
-  t.equal(result.type, "user", "handleMessage result.type must be 'user'");
-  t.equal(
-    result.user.action,
-    "create",
-    "handleMessage user.action must be 'create'"
-  );
+  await handleMessage(message);
 
   const user = await canvasApi.getUser(kthid);
-  t.ok(user, "User should exist in Canvas");
-  t.equal(
-    user.id,
-    result.user.id,
-    "Created user in Canvas should be same as value returned by handleMessage"
-  );
+  t.ok(user, `User with KTH ID ${kthid} should exist in Canvas`);
 });
 
 test("should not create a new user of affiliation:other in canvas", async (t) => {
-  t.plan(3);
+  t.plan(1);
   const kthid = randomstring.generate(8);
   const username = `${kthid}_abc`;
   const ladokId = randomstring.generate(24);
@@ -120,16 +89,14 @@ test("should not create a new user of affiliation:other in canvas", async (t) =>
     ladok3_student_uid: ladokId,
   };
 
-  const result = await handleMessage(message);
-  t.equal(result.type, "user", "handleMessage result.type must be 'user'");
-  t.equal(result.user.action, "ignore");
+  await handleMessage(message);
 
   const user = await canvasApi.getUser(kthid);
   t.equal(user, null, "User should not exist in Canvas");
 });
 
 test("should update a user in canvas", async (t) => {
-  t.plan(4);
+  t.plan(1);
   const kthid = "emiluppdaterar-namn";
   const username = `${kthid}_abc`;
   const ladokId = randomstring.generate(24);
@@ -141,7 +108,7 @@ test("should update a user in canvas", async (t) => {
     affiliation: ["student"],
     username,
     family_name: "Stenberg",
-    given_name: "Emil Stenberg",
+    given_name: "Emil",
     primary_email: "esandin@gmail.com",
     ladok3_student_uid: ladokId,
   };
@@ -152,29 +119,21 @@ test("should update a user in canvas", async (t) => {
     deleted: false,
     affiliation: ["student"],
     username,
-    family_name: "Stenberg",
-    given_name: "Emil Stenberg Uppdaterad",
+    family_name: "Stenberg Uppdaterad",
+    given_name: "Emil",
     primary_email: "esandin@gmail.com",
     ladok3_student_uid: ladokId,
   };
 
   await handleMessage(message);
-  const result = await handleMessage(message2);
-
-  t.equal(result.type, "user", "handleMessage result.type must be 'user'");
-  t.equal(result.user.action, "update");
+  await handleMessage(message2);
 
   const user = await canvasApi.getUser(kthid);
-  t.ok(user, "User should exist in Canvas");
-  t.equal(
-    user.id,
-    result.user.id,
-    "Updated user in Canvas should be same as value returned by handleMessage"
-  );
+  t.equal(user.name, "Emil Stenberg Uppdaterad", "Name should be updated");
 });
 
 test("should update a user in canvas even if Ladok ID is not supplied", async (t) => {
-  t.plan(4);
+  t.plan(2);
   const kthid = "emiluppdaterar-namn";
   const username = `${kthid}_abc`;
   const ladokId = randomstring.generate(24);
@@ -186,7 +145,7 @@ test("should update a user in canvas even if Ladok ID is not supplied", async (t
     affiliation: ["student"],
     username,
     family_name: "Stenberg",
-    given_name: "Emil Stenberg",
+    given_name: "Emil",
     primary_email: "esandin@gmail.com",
     ladok3_student_uid: ladokId,
   };
@@ -198,21 +157,14 @@ test("should update a user in canvas even if Ladok ID is not supplied", async (t
     affiliation: ["student"],
     username,
     family_name: "Stenberg",
-    given_name: "Emil Stenberg Uppdaterad",
+    given_name: "Emil Uppdaterad",
     primary_email: "esandin@gmail.com",
   };
 
   await handleMessage(message);
-  const result = await handleMessage(message2);
-
-  t.equal(result.type, "user", "handleMessage result.type must be 'user'");
-  t.equal(result.user.action, "update");
+  await handleMessage(message2);
 
   const user = await canvasApi.getUser(kthid);
   t.ok(user, "User should exist in Canvas");
-  t.equal(
-    user.id,
-    result.user.id,
-    "Updated user in Canvas should be same as value returned by handleMessage"
-  );
+  t.equal(user.name, "Emil Uppdaterad Stenberg", "Name should be updated");
 });
